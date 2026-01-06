f6cebd8b89cb6013ab1c8eebb6a5121502babfc2
Sébastien Strebelle <sebastien.strebelle@vinci.be>
2026-01-06 18:41:12 +0100

    examen js

---
+++ backend/services/questions.ts
@@ -0,0 +1,7 @@
+const questions = [
+    "Quels sont selon vous les points forts de notre service ?",
+    "Quels aspects de notre service pourraient être améliorés ?",
+    "Comment évalueriez-vous la qualité de notre support client ?",
+    "Recommanderiez-vous notre service à d'autres personnes ? Pourquoi ou pourquoi pas ?",
+    "Y a-t-il des fonctionnalités ou des services supplémentaires que vous aimeriez voir ?",
+]
